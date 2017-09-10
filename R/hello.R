e5426d96ad9455e96507b568e142bae4a8337d8f
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-09-10 22:48:52 +0200

    initial

---
+++ R/hello.R
@@ -0,0 +1,12 @@
+#' a fake function
+#'
+#' Document here. Nr. 3. One more
+#' I wonder how long this can be. [base::c()], [base::c], [get_fake()]
+#'
+#' @param x
+#' this is a param
+#' @export
+get_fake <- function(x) {
+  1
+}
+
